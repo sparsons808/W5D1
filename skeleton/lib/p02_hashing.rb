60acabc8fc19e317fb6af161c641e1a503df2108
ccy1563 <ccy1563@gmail.com>
2021-07-12 15:02:08 -0400

    Finished hashing

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,16 @@ end
 
 class Array
   def hash
+    self.map(&:to_s).join("").to_i
   end
 end
 
 class String
   def hash
+    alphabet = ("A".."z").to_a
+    new_str = []
+    self.each_char {|char| new_str << alphabet.index(char)}
+    new_str.join.to_i
   end
 end
 
@@ -16,6 +21,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    new_arr = []
+    self.keys.each {|k| new_arr << k.object_id}
+    new_arr.sum
   end
 end
