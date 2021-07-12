344c3a10df3b75dc14f346ddebcdbbfa25ee6ee2
Sydney <sydneyparsonsyb@gmail.com>
2021-07-12 15:40:30 -0400

    hash-set

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -1,23 +1,36 @@
 class HashSet
-  attr_reader :count
-
+  attr_reader :count, :store
+  attr_writer :count
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
   def insert(key)
+    if !self.include?(key)
+      resize! if @count == @store.size
+      self[key] << key
+      @count += 1
+      return true
+    end
+    false
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
+    self.store[num.hash % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -26,5 +39,22 @@ class HashSet
   end
 
   def resize!
+    num = self.store.size
+  
+    num.times do
+    
+      self.store << []
+    end
+  
+    (0...num).each do |i|
+    
+      self.store[i].each do |n|
+        temp = n
+        self.store[i].delete(n)
+      
+        self[temp] << temp
+      end
+    
+    end
   end
 end
